7243c3ce9de8fbf950fbc3218f07e5f991052145
Anoop Singh Rawat <112460114+Anoop-Rawat@users.noreply.github.com>
2023-04-18 12:52:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-07d3a50bd29811cd1"
   instance_type = "t2.micro"
